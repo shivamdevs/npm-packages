07f31429f6d70d70a9ba1a7e2ca2ac86bd096d16
Shivam Dewangan <153713550+shivam-ttt@users.noreply.github.com>
2024-04-15 16:12:05 +0530

    Package updated react-unique-hooks 0.1.23

--- react-unique-hooks/src/hooks/useAsync/index.ts
+++ react-unique-hooks/src/hooks/useAsync/index.ts
@@ -3,35 +3,36 @@
 import { useCallback, useEffect, useState } from "react";
 import { Dependencies, ErrorHook } from "../../util";
 
-type PromiseCallback<T> = () => Promise<T>;
+type PromiseCallback<T> = (() => Promise<T>) | null;
 
 export type AsyncHook<T, E> = [
-  value: T | undefined,
-  loading: boolean,
-  error: ErrorHook<E>
+	value: T | undefined,
+	loading: boolean,
+	error: ErrorHook<E>
 ];
 
 export default function useAsync<T, E>(
-  callback: PromiseCallback<T>,
-  dependencies: Dependencies = []
+	callback: PromiseCallback<T>,
+	dependencies: Dependencies = []
 ): AsyncHook<T, E> {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<ErrorHook<E>>();
-  const [value, setValue] = useState<T | undefined>();
+	const [loading, setLoading] = useState<boolean>(callback !== null);
+	const [error, setError] = useState<ErrorHook<E>>();
+	const [value, setValue] = useState<T | undefined>();
 
-  const callbackMemoized = useCallback(() => {
-    setLoading(true);
-    setError(undefined);
-    setValue(undefined);
-    callback()
-      .then(setValue)
-      .catch((e: ErrorHook<E>) => setError(e))
-      .finally(() => setLoading(false));
-  }, dependencies);
+	const callbackMemoized = useCallback(() => {
+		if (callback === null) return;
+		setLoading(true);
+		setError(undefined);
+		setValue(undefined);
+		callback()
+			.then(setValue)
+			.catch((e: ErrorHook<E>) => setError(e))
+			.finally(() => setLoading(false));
+	}, dependencies);
 
-  useEffect(() => {
-    callbackMemoized();
-  }, [callbackMemoized]);
+	useEffect(() => {
+		callbackMemoized();
+	}, [callbackMemoized]);
 
-  return [value, loading, error];
+	return [value, loading, error];
 }
